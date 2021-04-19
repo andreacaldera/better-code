f4e2012d9d0ebfd9288021f7c65fb5779f678ea9
Andrea Caldera <andrea.caldera@gmail.com>
2021-04-19 23:16:53 +0100

    lint staged

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import { AwesomeComponent } from './awesome-component';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { AwesomeComponent } from "./awesome-component";
 
 function App() {
   return (
