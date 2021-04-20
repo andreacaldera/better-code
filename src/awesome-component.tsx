054f9531c2a43490d41aa66ea7b80c82ed6c59a3
Andrea Caldera <andrea.caldera@gmail.com>
2021-04-20 09:52:43 +0100

    test staged formatting

--- src/awesome-component.tsx
+++ src/awesome-component.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import { useDemoHook } from './useDemoHook'
+import React, { useEffect, useState } from "react";
+import { useDemoHook } from "./useDemoHook";
 
-const NAUGHTY_DEVELOPER = false
+const NAUGHTY_DEVELOPER = false;
 
 export const AwesomeComponent: React.FunctionComponent = () => {
-  const { demoPromise } = useDemoHook()
-  const [state, setState] = useState<string>()
+  const { demoPromise } = useDemoHook();
+  const [state, setState] = useState<string>();
 
   useEffect(() => {
-    demoPromise().then(setState)
-  }, [])
+    demoPromise().then(setState);
+  }, []);
 
   if (NAUGHTY_DEVELOPER) {
-    return null
+    return null;
     /** catch error faster by using Typescript, the following line is incorrect and will immediately give you an error instead of failing at runtime */
     // return undefined
   }
-  return <h1>Typescript is awesome: {state}</h1>
-}
+  return <h1>Typescript is awesome: {state}</h1>;
+};
